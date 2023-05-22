a862bd91caec60dd8686102ab130cb95ca0b1368
tanyaKalra-8 <108450417+tanyaKalra-8@users.noreply.github.com>
2023-05-22 17:57:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+let elements = document.getElementsByTagName("strong")
 function highlight() {
     //Write your code here
-
-
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].style.color = "green";
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].style.color = "";
+	}
 }
